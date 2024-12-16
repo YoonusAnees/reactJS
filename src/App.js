6612a16494226ee29e2cd88bd8279485595e4142
YoonusAnees <yoonusaneesniis@gmail.com>
2024-12-17 01:01:09 +0530

    reactapp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+
+  // function namechange () {
+  //   const name = ["Earn","Grow","Give"];
+  //   const int = Math.floor (Math.random()*3);
+  //   return name[int];
+  // }
+
+  return (
+    <div>
+      <header/>
+    </div>
+   
+   
+  );
+}
+
+export default App;
